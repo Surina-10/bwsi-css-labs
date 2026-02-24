31135ebdec6cdee8e2eaecb8422ce3b9bbe9a1cf
Surina-10 <surina.yadav10@gmail.com>
2026-02-23 20:37:04 -0600

    Add intro to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,11 @@ def main():
     name = "Surina Yadav" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi everyone! My name is Surina Yadav and I am a junior at "\
+          "Glenbrook North High School in Illinois. I'm interested in robotics" \
+            " and am part of an FTC team (10138, Newton Busters). I am also in a Scouting troop"\
+                " and love to be outdoors (achieved Eagle last summer!). Beyond that I enjoy"  \
+                " 3D printing and reading fantasy and sci-fi. Can't wait to learn with you all!")
 
 if __name__ == "__main__":
     main()
